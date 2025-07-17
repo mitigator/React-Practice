8d2fa5e1019345a9800fb5e7d1ce296fef8e5ce8
Shubham <shubhammishra4623@gmail.com>
2025-07-17 14:58:10 +0530

    Counter App

--- Counter App/src/App.jsx
+++ Counter App/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem("count");
+    return savedCount ? parseInt(savedCount) : 0;
+  });
+
+  const increment = () => setCount((prev) => (prev < 10 ? prev + 1 : prev));
+  const decrement = () => setCount((prev) => (prev > 0 ? prev - 1 : prev));
+  const reset = () => setCount(0);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "ArrowUp") increment();
+      if (e.key === "ArrowDown") decrement();
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  });
+
+  useEffect(() => {
+    const savedCount = localStorage.getItem("count");
+    if (savedCount) setCount(parseInt(savedCount));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Counter App</h1>
+        <h2>{count}</h2>
+        <div className="buttoncontainer">
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+          <button onClick={reset}>Reset</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
